e05567e904a11f69dd3b47eaddc5bef58ca4c9ed
Braingenius2 <braingenius2@gmail.com>
2023-07-12 14:33:40 +0300

    Configure routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import './css/App.css';
+import Navbar from './components/Navbar';
+
+const Layout = () => (
+  <div className="container">
+    <Navbar />
+    <Outlet />
+  </div>
+);
 
 const App = () => (
-  <section id="calculator-container">
-    <Calculator />
-    <Quote />
-  </section>
+  <BrowserRouter>
+    <div className="content">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Route>
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
